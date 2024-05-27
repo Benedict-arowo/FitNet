c8a49b22f61ef99864970ab377905a03fb048fea
Benedict <benedict.arowo@gmail.com>
2024-05-27 12:09:43 +0300

    chore: Update npm dependencies, add waitlist validation, and remove unnecessary field

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,40 +1,9 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
-
-console.log();
+import Waitlist from "./pages/waitlist";
 function App() {
-	const [position, setPosition] = useState([6.5244, 3.3792]);
-
-	useEffect(() => {
-		navigator.geolocation.getCurrentPosition((position) => {
-			setPosition([position.coords.latitude, position.coords.longitude]);
-		});
-	}, []);
-
-	useEffect(() => {
-		console.log(position);
-	}, [position]);
-
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">MakeFit!</h1>
-			<MapContainer
-				className="h-[500px]"
-				center={position}
-				zoom={13}
-				scrollWheelZoom={true}>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				<Marker position={position}>
-					<Popup>
-						You are here! <br /> Easily customizable.
-					</Popup>
-				</Marker>
-			</MapContainer>
-		</div>
+		<main className="h-full min-h-full">
+			<Waitlist />
+		</main>
 	);
 }
-
 export default App;
