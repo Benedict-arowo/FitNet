a55ee13219b4ad18f4827b47a340e005634e096f
Benedict <benedict.arowo@gmail.com>
2024-06-10 09:13:49 +0300

    chore: Removed unnecessary imports in updateGyms function, moved DB/FITNESS.ERD/index,js to the root folder(/DB)

--- Backend/jobs/updateGyms.js
+++ Backend/jobs/updateGyms.js
@@ -1,19 +1,18 @@
 // jobs/updateGyms.js
-const cron = require('node-cron');
-const fetchAndUpdateGyms = require('../scripts/fetchGyms'); // Adjust path as necessary
-const db = require('../models'); // Import the database models
+const cron = require("node-cron");
+const fetchAndUpdateGyms = require("../scripts/fetchGyms"); // Adjust path as necessary
+// const db = require("../models"); // Import the database models
 
 // Schedule the job to run every day at midnight
-cron.schedule('0 0 * * *', async () => {
-  console.log('Running the gym update job...');
+cron.schedule("0 0 * * *", async () => {
+	console.log("Running the gym update job...");
 
-  try {
-    await fetchAndUpdateGyms();
-    console.log('Gym data updated successfully');
-  } catch (error) {
-    console.error('Error updating gym data:', error);
-  }
+	try {
+		await fetchAndUpdateGyms();
+		console.log("Gym data updated successfully");
+	} catch (error) {
+		console.error("Error updating gym data:", error);
+	}
 });
 
-console.log('Cron job scheduled to update gym data daily at midnight');
-
+console.log("Cron Job is up and running!!!");
